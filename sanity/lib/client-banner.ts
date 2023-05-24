3d36115c5b3b8e5f1f236b2f4f50902e59b5374b
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-05-24 16:10:58 -0600

    Create banner and product clients

---
+++ sanity/lib/client-banner.ts
@@ -0,0 +1,23 @@
+import { createClient, groq } from "next-sanity";
+import { Banner } from "@/types/Banner";
+import config from "../config/client-config";
+
+export async function getBanners(): Promise<Banner[]> {
+  return createClient(config).fetch(
+    groq`*[_type == "banner"]{
+                _id,
+                _createdAt,
+                _updatedAt,
+                "image": image.asset->url,
+                buttonText,
+                product,
+                desc,
+                smallText,
+                midText,
+                largeText1,
+                largeText2,
+                discount,
+                saleTime
+            }`
+  );
+}
---
+++ sanity/lib/client-product.ts
@@ -0,0 +1,34 @@
+import { createClient, groq } from "next-sanity";
+import { Product } from "@/types/Product";
+import config from "../config/client-config";
+
+export async function getProducts(): Promise<Product[]> {
+  return createClient(config).fetch(
+    groq`*[_type == "product"]{
+            _id,
+            _createdAt,
+            _updatedAt,
+            "image": image.asset->url,
+            name,
+            "slug": slug.current,
+            price,
+            details
+        }`
+  );
+}
+
+export async function getProduct(slug: string): Promise<Product> {
+  return createClient(config).fetch(
+    groq`*[_type == "product" && slug.current == $slug][0]{
+            _id,
+            _createdAt,
+            _updatedAt,
+            "image": image.asset->url,
+            name,
+            "slug": slug.current,
+            price,
+            details
+        }`,
+    { slug }
+  );
+}
