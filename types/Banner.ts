0e10e1411fa566febef1929109977e8d12533c52
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-05-24 00:59:17 -0600

    Add banner type

---
+++ types/Banner.ts
@@ -0,0 +1,15 @@
+export type Banner = {
+  _id: string;
+  _createdAt: string;
+  _updatedAt: string;
+  image: string;
+  buttonText: string;
+  product: string;
+  desc: string;
+  smallText: string;
+  midText: string;
+  largeText1: string;
+  largeText2: string;
+  discount: string;
+  saleTime: string;
+};
