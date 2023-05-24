f30b70bffe78f171b4ca51cc49360ff390c4d16a
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-05-24 01:36:12 -0600

    Move files into folders for better organization

---
+++ app/(site)/page.tsx
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <>
+      HeroBanner
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+        {["Product 1", "Product 2", "Product 3", "Product 4"].map(
+          (product) => product
+        )}
+      </div>
+      Footer
+    </>
+  );
+};
+
+export default Home;
