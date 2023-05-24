5e2eaf6732cb2dc684c1722a308139c76f3124e1
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-05-24 00:53:48 -0600

    Create config file

---
+++ sanity.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import schemas from "./sanity/schemas";
+
+export default defineConfig({
+  basePath: "/studio",
+  projectId: "ic8uro0v",
+  title: "eCommerce Sanity Studio",
+  dataset: "production",
+  apiVersion: "2023-05-23",
+  plugins: [deskTool()],
+  schema: { types: schemas },
+});
